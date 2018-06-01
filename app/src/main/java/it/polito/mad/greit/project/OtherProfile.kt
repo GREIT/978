93fb7b3a4f6007f920a67166710110e167302b9c
Carmine D'Amico <carmine920@gmail.com>
2018-06-01 10:08:19 +0200

    divider between reviews

--- app/src/main/java/it/polito/mad/greit/project/OtherProfile.kt
+++ app/src/main/java/it/polito/mad/greit/project/OtherProfile.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.ImageView
@@ -96,6 +97,7 @@ class OtherProfile : AppCompatActivity() {
   fun setupReviewsList (uid : String) {
     val layoutManager = LinearLayoutManager(this)
     layoutManager.reverseLayout = false
+    mReviewsList!!.addItemDecoration(DividerItemDecoration(mReviewsList!!.context, layoutManager.orientation))
     mReviewsList!!.setHasFixedSize(true)
     mReviewsList!!.layoutManager = layoutManager
 
