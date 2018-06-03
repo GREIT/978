0ef1d25aa06f8c971ea09973b7307dbf39641081
Carmine D'Amico <carmine920@gmail.com>
2018-06-03 16:05:51 +0200

    positions altered if equal

--- app/src/main/java/it/polito/mad/greit/project/SharedBooksOnMap.java
+++ app/src/main/java/it/polito/mad/greit/project/SharedBooksOnMap.java
@@ -28,16 +28,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 
 public class SharedBooksOnMap extends Fragment {
   GoogleMap myMap;
   MapView mMapView;
   String ISBN;
   String currentLocation;
+  HashMap<String, String> markerLocation;
   
   private DatabaseReference mSharedBookDb;
   private static final String ARG_PARAM1 = "isbn";
   private static final String ARG_PARAM2 = "location";
+  private static final float COORDINATE_OFFSET = 0.00002f;
+  private static final int MAX_NUMBER_OF_MARKERS = 10;
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,8 @@ public class SharedBooksOnMap extends Fragment {
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_shared_books_on_map, container, false);
   
+    markerLocation = new HashMap<>();
+    
     mMapView = (MapView) rootView.findViewById(R.id.mapView);
     mMapView.onCreate(savedInstanceState);
   
@@ -83,7 +90,16 @@ public class SharedBooksOnMap extends Fragment {
             for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
               SharedBook sb = snapshot.getValue(SharedBook.class);
               String[] sbCoordinates = sb.getCoordinates().split(";");
-              LatLng sbPosition = new LatLng(Double.valueOf(sbCoordinates[0]), Double.valueOf(sbCoordinates[1]));
+              
+              if (!mapAlreadyHasMarkerForLocation(Float.valueOf(sbCoordinates[0]) + ";" + Float.valueOf(sbCoordinates[1]))) markerLocation.put((Float.valueOf(sbCoordinates[0]) + ";" + Float.valueOf(sbCoordinates[1])), "");
+              else {
+                sbCoordinates = coordinateForMarker(Float.valueOf(sbCoordinates[0]), Float.valueOf(sbCoordinates[1]));
+                markerLocation.put((Float.valueOf(sbCoordinates[0]) + ";" + Float.valueOf(sbCoordinates[1])), "");
+              }
+              
+              System.out.println(sbCoordinates[0] + "\t" + sbCoordinates[1]);
+              
+              LatLng sbPosition = new LatLng(Float.valueOf(sbCoordinates[0]), Float.valueOf(sbCoordinates[1]));
               Marker mk =  mMap.addMarker(new MarkerOptions().position(sbPosition));
               mk.setTag((SharedBook) sb);
             }
@@ -122,10 +138,39 @@ public class SharedBooksOnMap extends Fragment {
       }
     });
     
+    
+    
     return rootView;
   }
   
+  private String[] coordinateForMarker(float latitude, float longitude) {
+    
+    String[] location = new String[2];
+    float lat = latitude, lon = longitude;
+    int k = 1;
+    
+    location[0] = lat + "";
+    location[1] = lon + "";
+    
+    while (mapAlreadyHasMarkerForLocation(lat + ";" + lon)) {
+      if (k % 2 == 0) {
+        lat += (k * COORDINATE_OFFSET);
+        lon += (k * COORDINATE_OFFSET);
+      } else {
+        lat -= (k * COORDINATE_OFFSET);
+        lon -= (k * COORDINATE_OFFSET);
+      }
+      k++;
+      location[0] = lat + "";
+      location[1] = lon + "";
+    }
+    
+    return location;
+  }
   
+  private boolean mapAlreadyHasMarkerForLocation(String location) {
+    return (markerLocation.containsKey(location));
+  }
   
   @Override
   public void onResume() {
