ae0da260f1fa0c87d3d707b2a8a463638ce2fa41
marco-bilardi <marcobilardi@gmail.com>
2018-06-04 12:27:48 +0200

    Fixed update token

--- app/src/main/java/it/polito/mad/greit/project/MainEmptyActivity.java
+++ app/src/main/java/it/polito/mad/greit/project/MainEmptyActivity.java
@@ -3,12 +3,15 @@ package it.polito.mad.greit.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainEmptyActivity extends AppCompatActivity {
   
@@ -28,6 +31,10 @@ public class MainEmptyActivity extends AppCompatActivity {
     if (user == null) {
       intent = new Intent(this, SignInActivity.class);
     } else {
+      DatabaseReference dbref = FirebaseDatabase.getInstance().getReference("TOKENS").child(user.getUid());
+      String token = FirebaseInstanceId.getInstance().getToken();
+      dbref.setValue(token);
+      Log.d("MyFirebaseIIDService", "onCreate: sent token " + token);
       intent = new Intent(this, MainActivity.class);
     }
 
